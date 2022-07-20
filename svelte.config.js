7a2e65b172cf1732624a2c735e0d067bc16f0b2a
r6q <17678057+r6q@users.noreply.github.com>
2022-07-20 22:35:11 +0300

    🎉 initial

---
+++ svelte.config.js
@@ -0,0 +1,30 @@
+import adapter from '@sveltejs/adapter-static'
+import preprocess from 'svelte-preprocess'
+
+const environment = 'prod'
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
+
+  kit: {
+    adapter: adapter(
+        {
+          pages: 'build',
+          assets: 'build',
+        },
+    ),
+
+    paths: {
+      base: environment === 'prod' ? '/sveltekit-demo1' : '/abc',
+    },
+
+    prerender: {
+      default: true,
+    },
+  },
+}
+
+export default config
