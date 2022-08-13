4c9b12cdf21f83a11b8cee6e0f5eba5de34c4f01
r6q <17678057+r6q@users.noreply.github.com>
2022-08-13 15:00:29 +0300

    💄 improve layout of book details view

--- src/lib/BookDetails.svelte
+++ src/lib/BookDetails.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import type {BookData} from './data'
-  import { base } from '$app/paths';
+  import {base} from '$app/paths'
 
   export let key = ''
   export let book: BookData | undefined = undefined
@@ -9,12 +9,17 @@
 
 <div class="s-book-details-container">
   <div class="s-details-left">
-    <img src="{base}/images/{key}/{key}_front.jpg" alt={book?.title} class="s-card-image">
+    <img src="{base}/images/{key}/{key}_front.jpg" alt={book?.title} class="s-main-img">
   </div>
   <div class="s-details-right">
-    {book?.title}
-    {@html book?.description}
-    {book?.storeUrl}
+    <div class="s-book-title title">{book?.title}</div>
+    <div>
+      {@html book?.description}
+    </div>
+    <a id="buy-button" class="button is-info" href={book?.storeUrl} target="_blank">
+      Apskatīt internetveikalā
+    </a>
+    <!--{book?.storeUrl}-->
   </div>
 </div>
 
@@ -22,23 +27,39 @@
   .s-book-details-container {
     display: grid;
     grid-template-columns: repeat(2, 1fr);
+    overflow: hidden;
   }
 
-  .s-card-image {
-    width: 100%;
+  .s-main-img {
+    width: 98%; // for some reason it add 8 extra pixels compared to parent div
+    padding: 1rem 1rem 0 1rem;
   }
 
   .s-details-left {
-    padding: 2rem;
+
+  }
+
+  .s-book-title {
+    margin-bottom: 0;
   }
 
   .s-details-right {
-    padding: 2rem;
+    & > * {
+      margin: 1rem 1rem 1rem 1rem;
+    }
   }
 
   @media screen and (max-width: 576px) {
     .s-details-left {
       grid-column: span 2;
+    }
+
+    .s-details-right {
+      grid-column: span 2;
+    }
+
+    #buy-button {
+      width: 92%;
     }
   }
 </style>
